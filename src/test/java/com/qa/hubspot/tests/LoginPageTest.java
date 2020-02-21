71885b3e5960ab8605dca61109ba7dd6d4b7b8f9
Saurav Saha <srv321@gmail.com>
2020-02-21 22:39:08 +0530

    adding log4j code

--- src/test/java/com/qa/hubspot/tests/LoginPageTest.java
+++ src/test/java/com/qa/hubspot/tests/LoginPageTest.java
@@ -2,6 +2,7 @@ package com.qa.hubspot.tests;
 
 import java.util.Properties;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
@@ -31,6 +32,8 @@ public class LoginPageTest {
 	Properties prop;
 	LoginPage loginPage;
 	Credentials userCred;
+	
+	Logger log = Logger.getLogger(LoginPageTest.class);
 
 
 	@BeforeTest(alwaysRun=true)
@@ -40,7 +43,7 @@ public class LoginPageTest {
 		basePage = new BasePage();
 		prop = basePage.init_properties();
 		
-		if(browser.equals(null) || browser.equals("") || browser.isEmpty()){
+		if(browser.equals(null)){
 			 browserName = prop.getProperty("browser");
 		}else{
 			browserName = browser;
@@ -48,6 +51,7 @@ public class LoginPageTest {
 
 		driver = basePage.init_driver(browserName);
 		driver.get(prop.getProperty("url"));
+		log.info("url is launched: " + prop.getProperty("url"));
 		loginPage = new LoginPage(driver);
 		userCred = new Credentials(prop.getProperty("username"), prop.getProperty("password"));
 
@@ -57,17 +61,24 @@ public class LoginPageTest {
 	@Description("verify Login Page Title Test....")
 	@Severity(SeverityLevel.NORMAL)
 	public void verifyLoginPageTitleTest() throws InterruptedException {
+		log.info("starting---------------------->>>>>>>>>>verifyLoginPageTitleTest");
 		String title = loginPage.getPageTitle();
 		System.out.println("login page title is: " + title);
+		log.info("login page title is: " + title);
 		Assert.assertEquals(title, AppConstants.LOGIN_PAGE_TITLE);
-
+		log.info("ending---------------------->>>>>>>>>>verifyLoginPageTitleTest");
+		log.warn("some warning");
+		log.error("some error");
+		log.fatal("fatal error");
 	}
 
 	@Test(priority = 2, groups = "sanity")
 	@Description("verify Sign up link Test....")
 	@Severity(SeverityLevel.CRITICAL)
 	public void verifySignUpLinkTest() {
+		log.info("starting---------------------->>>>>>>>>>verifySignUpLinkTest");
 		Assert.assertTrue(loginPage.checkSignUpLink());
+		log.info("ending---------------------->>>>>>>>>>verifySignUpLinkTest");
 
 	}
 
@@ -75,9 +86,11 @@ public class LoginPageTest {
 	@Description("verify Login Test....")
 	@Severity(SeverityLevel.BLOCKER)
 	public void loginTest() {
+		log.info("starting---------------------->>>>>>>>>>loginTest");
 		HomePage homePage = loginPage.doLogin(userCred);
 		String accountName = homePage.getLoggedInUserName();
 		Assert.assertEquals(accountName, prop.getProperty("accountname"));
+		log.info("ending---------------------->>>>>>>>>>loginTest");
 
 	}
 
@@ -95,10 +108,12 @@ public class LoginPageTest {
 
 	@Test(priority =4,dataProvider = "getLoginInvalidData", enabled =false)
 	public void login_InvalidTestCases(String username, String pwd) {
+		log.info("starting---------------------->>>>>>>>>>login_InvalidTestCases");
 		userCred.setAppUserName(username);
 		userCred.setAppUserPassword(pwd);
 		loginPage.doLogin(userCred);
 		Assert.assertTrue(loginPage.checkLoginErrorMesg());
+		log.info("ending---------------------->>>>>>>>>>login_InvalidTestCases");
 	}
 
 	@AfterTest(alwaysRun=true)
